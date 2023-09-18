bf2745bfdbce1a1365789a4ecbb32deac1d1a59a
Roman Ptashnyk <2508romaa@gmail.com>
2023-09-18 18:33:25 +0300

    commit

--- src/components/Partners/PartnerItems.tsx
+++ src/components/Partners/PartnerItems.tsx
@@ -1,11 +1,11 @@
 // @ts-nocheck
 
-import React from "react"
-import cn from "classnames"
+import React from 'react'
+import cn from 'classnames'
 
-import styles from "./Partners.module.scss"
-import { GetPartnersQuery } from "@/graphql/__generated__"
-import PartnerItem from "./PartnerItem"
+import styles from './Partners.module.scss'
+import { GetPartnersQuery } from '@/graphql/__generated__'
+import PartnerItem from './PartnerItem'
 
 interface IPartnerItemsProps {
   partners: GetPartnersQuery
@@ -21,40 +21,28 @@ const PartnerItems: React.FC<IPartnerItemsProps> = ({ partners }) => {
 
   return (
     <>
-      <div className={styles["partners__items-wrap"]}>
-        <div className={cn(styles["partners__items"], styles["marquee"])}>
+      <div className={styles['partners__items-wrap']}>
+        <div className={cn(styles['partners__items'], styles['marquee'])}>
           {slicedArray[0].map((partner) => (
-            <PartnerItem partner={partner} />
+            <PartnerItem partner={partner} key={partner.id} />
           ))}
         </div>
-        <div className={cn(styles["partners__items"], styles["marquee"])}>
+        <div className={cn(styles['partners__items'], styles['marquee'])}>
           {slicedArray[1].map((partner) => (
-            <PartnerItem partner={partner} />
+            <PartnerItem partner={partner} key={partner.id} />
           ))}
         </div>
       </div>
 
-      <div className={styles["partners__items-wrap"]}>
-        <div
-          className={cn(
-            styles["partners__items"],
-            styles["marquee"],
-            styles["reverce"]
-          )}
-        >
+      <div className={styles['partners__items-wrap']}>
+        <div className={cn(styles['partners__items'], styles['marquee'], styles['reverce'])}>
           {slicedArray[2].map((partner) => (
-            <PartnerItem partner={partner} />
+            <PartnerItem partner={partner} key={partner.id} />
           ))}
         </div>
-        <div
-          className={cn(
-            styles["partners__items"],
-            styles["marquee"],
-            styles["reverce"]
-          )}
-        >
+        <div className={cn(styles['partners__items'], styles['marquee'], styles['reverce'])}>
           {slicedArray[3].map((partner) => (
-            <PartnerItem partner={partner} />
+            <PartnerItem partner={partner} key={partner.id} />
           ))}
         </div>
       </div>
